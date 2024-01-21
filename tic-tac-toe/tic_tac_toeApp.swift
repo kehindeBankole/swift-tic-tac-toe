5668187b63fd8716af617e0bc8f8f5a918768c5c
kehindeBankole <bankolek1@gmail.com>
2024-01-21 13:35:02 +0100

    Initial Commit

---
+++ tic-tac-toe/tic_tac_toeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tic_tac_toeApp.swift
+//  tic-tac-toe
+//
+//  Created by kehinde on 21/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct tic_tac_toeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
