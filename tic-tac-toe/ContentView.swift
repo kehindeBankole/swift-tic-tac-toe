c2b88b4d9401514751087b461de3f1a5f3918c0b
kehindeBankole <bankolek1@gmail.com>
2024-01-22 22:14:42 +0100

    calculate score

--- tic-tac-toe/ContentView.swift
+++ tic-tac-toe/ContentView.swift
@@ -19,9 +19,7 @@ struct ContentView: View {
     
     func horizontalScore(arr:[[String]])  {
         for val in arr {
-            if(val.allSatisfy{$0 == val.first}){
-                print(val)
-                winner = val[0]
+            if(val.allSatisfy{$0 == val.first}){                winner = val[0]
                 playerWin = true
                 row = [["1","2","3"] , ["4","5","6"] , ["7","8","9"]]
             }
@@ -30,23 +28,56 @@ struct ContentView: View {
     }
     
     func verticalScore(){
-  
+        
         var newRow:[String] = []
         var y = 0
         repeat{
             for x in 0..<row.count {
                 newRow.append(row[x][y])
             }
-           y += 1
+            y += 1
         }while(y < 3)
-
+        
         let groupedArray = stride(from: 0, to: newRow.count, by: 3).map { startIndex in
-
+            
             Array(newRow[startIndex..<min(startIndex + 3, newRow.count)])
         }
         horizontalScore(arr: groupedArray)
-
+        
     }
+    
+    func diagonalScore(matrix: [[String]]) {
+        var diagonalElements: [String] = []
+        var reverseDiagonalElements: [String] = []
+        
+        for i in 0..<matrix.count {
+            print(matrix[i][i] , i)
+            if i < matrix[i].count {
+                diagonalElements.append(matrix[i][i])
+            }
+        }
+        
+        for i in 0..<matrix.count {
+              let columnIndex = matrix[i].count - 1 - i
+              if columnIndex >= 0 {
+                  reverseDiagonalElements.append(matrix[i][columnIndex])
+              }
+          }
+
+        
+        if(diagonalElements.allSatisfy{$0 == diagonalElements.first}){
+            playerWin = true
+            winner = diagonalElements[0]
+            row = [["1","2","3"] , ["4","5","6"] , ["7","8","9"]]
+        }
+        if(reverseDiagonalElements.allSatisfy{$0 == reverseDiagonalElements.first}){
+            playerWin = true
+            winner = reverseDiagonalElements[0]
+            row = [["1","2","3"] , ["4","5","6"] , ["7","8","9"]]
+        }
+    }
+    
+
     
     var body: some View {
         VStack {
@@ -54,22 +85,23 @@ struct ContentView: View {
                 HStack{
                     
                     ForEach(Array(rowItem.enumerated()) , id:\.element){colIndex , col in
-                            Button(action: {
-                                
-                                if(row[rowIndex][colIndex]=="X" || row[rowIndex][colIndex]=="O"){
-                                    return
-                                }else{
-                                    row[rowIndex][colIndex]=currentPlayer.rawValue
-                                }
-                                if(currentPlayer == .O){
-                                    currentPlayer = .X
-                                }else{
-                                    currentPlayer = .O
-                                }
-                                horizontalScore(arr: row)
-                                verticalScore()
-                            }){
-                                Text(col).foregroundStyle(.white).opacity((col != "O" && col != "X") ? 0 : 1)     .background(Rectangle().fill(.black).cornerRadius(15).frame(width: 70, height: 70)).padding()               }
+                        Button(action: {
+                            
+                            if(row[rowIndex][colIndex]=="X" || row[rowIndex][colIndex]=="O"){
+                                return
+                            }else{
+                                row[rowIndex][colIndex]=currentPlayer.rawValue
+                            }
+                            if(currentPlayer == .O){
+                                currentPlayer = .X
+                            }else{
+                                currentPlayer = .O
+                            }
+                            horizontalScore(arr: row)
+                            verticalScore()
+                            diagonalScore(matrix: row)
+                        }){
+                            Text(col).foregroundStyle(.white).opacity((col != "O" && col != "X") ? 0 : 1)     .background(Rectangle().fill(.black).cornerRadius(15).frame(width: 70, height: 70)).padding()               }
                         
                     }.padding()
                 }
